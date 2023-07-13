6a2be0357d7975b7e0a62815b916d1be3d95416a
Aryan-9211 <manglaaryan178@gmail.com>
2023-07-13 23:30:10 +0530

    initialised the project and blog app

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from froala_editor.fields import FroalaField
+
+# Create your models here.
+class BlogModel(models.Model):
+    title = models.CharField(max_length=1000)
+    content = FroalaField()
+    image = models.ImageField(upload_to='static/images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upload_to = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
