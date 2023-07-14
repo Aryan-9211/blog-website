51d2151e0adb8dd1d3b3c6cb5f3bab0ba063bf6f
Aryan-9211 <manglaaryan178@gmail.com>
2023-07-15 03:05:41 +0530

    added update and delete feature

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('add-blog/', views.add_blog, name="add_blog"), 
     path('blog-detail/<title>', views.blog_detail, name="blog_detail"),
     path('see-blog/', views.see_blog, name="see_blog"),
+    path('blog-delete/<id>', views.blog_delete, name="blog_delete"),
+    path('blog-update/<title>', views.blog_update, name="blog_update"),
+
 ] 
--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,15 @@ def blog_detail(request, title):
         print(e)
     return render(request, 'blog_detail.html', context)
 
+def see_blog(request):
+    context = {}
+    try:
+        blog_objs = BlogModel.objects.filter(user = request.user)
+        context['blog_objs'] = blog_objs
+    except Exception as e:
+        print(e)
+    return render(request, 'see_blogs.html', context)
+
 def add_blog(request):
     form = BlogForm()
     context = {'form': form}
@@ -38,16 +47,49 @@ def add_blog(request):
                 return redirect('/add-blog/')
 
     except Exception as e:
-        print("mangla")
         print(e)
     return render(request, 'add_blog.html', context)
 
 
-def see_blog(request):
-    context = {}
+def blog_delete(request, id):
     try:
-        blog_objs = BlogModel.objects.filter(user = request.user)
-        context['blog_objs'] = blog_objs
+        blog_obj = BlogModel.objects.get(id=id)
+        blog_obj.delete()
     except Exception as e:
         print(e)
-    return render(request, 'see_blogs.html', context)
+    return redirect('/see-blog/')
+
+
+
+def blog_update(request, title):
+    context = {}
+    try:
+        blog_obj = BlogModel.objects.get(title=title)
+        initial_dict = {'content': blog_obj.content}
+        form = BlogForm(initial=initial_dict)
+
+        if request.method == 'POST':
+            form = BlogForm(request.POST, request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data['image']
+                updated_title = request.POST.get('title')
+                user = request.user
+                content = form.cleaned_data['content']
+                
+                blog_obj.title = updated_title
+                blog_obj.content = content
+                blog_obj.image = image
+                blog_obj.save()
+                
+                return redirect('/add-blog/')
+
+        context['blog_obj'] = blog_obj 
+        context['form'] = form
+
+    except Exception as e:
+        print(e)    
+    
+    return render(request, 'update_blog.html', context)
+
+
+
