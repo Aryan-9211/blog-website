0303ea0a809f820328e3ae700ebcbc641c9fdc9a
Aryan-9211 <manglaaryan178@gmail.com>
2023-07-16 21:30:55 +0530

    updated login and register routes

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from blog import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login/", views.login, name="login"),
-    path('register/', views.register, name="register"),
+    path("login/", views.login, name="login_view"),
+    path('register/', views.register, name="register_view"),
     path('add-blog/', views.add_blog, name="add_blog"), 
     path('blog-detail/<title>', views.blog_detail, name="blog_detail"),
     path('see-blog/', views.see_blog, name="see_blog"),
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,7 @@
 function login() {
-  console.log("HI");
   var username = document.getElementById("loginUsername").value;
   var password = document.getElementById("loginPassword").value;
   var csrf = document.getElementById("csrf").value;
-
-  console.log(username, password, csrf);
 
   var data = {
     username: username,
@@ -27,6 +24,33 @@ function login() {
       }
     })
     .catch((error) => console.error(error));
+}
 
-  console.log("END");
+function register() {
+  console.log("HI");
+  var username = document.getElementById("loginUsername").value;
+  var password = document.getElementById("loginPassword").value;
+  var csrf = document.getElementById("csrf").value;
+
+  var data = {
+    username: username,
+    password: password,
+  };
+
+  fetch("/api/register/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrf,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result.status == 200) {
+        window.location.href = "/";
+      }
+    })
+    .catch((error) => console.error(error));
 }
